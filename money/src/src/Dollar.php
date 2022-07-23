b57ccb965c257e585660630d6b9a756b23dc61f2
kurita_k <kensuke.kurita@gmail.com>
2022-07-23 15:11:30 +0900

    create Dollar

---
+++ money/src/src/Dollar.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace KensukeKurita\Tdd\Money\src;
+
+class Dollar
+{
+
+    /**
+     * @param int $int
+     */
+    public function __construct(int $int)
+    {
+    }
+}
--- money/tests/Unit/MoneyTest.php
+++ money/tests/Unit/MoneyTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace KensukeKurita\Tdd\Money\Tests\Unit;
 
+use KensukeKurita\Tdd\Money\src\Dollar;
 use PHPUnit\Framework\TestCase;
 
 class MoneyTest extends TestCase
