4ca2bc0abf01bf964a0a8bff33f049a2f378532c
kurita_k <kensuke.kurita@gmail.com>
2022-07-25 15:44:10 +0900

    refactor test

--- money/tests/Unit/MoneyTest.php
+++ money/tests/Unit/MoneyTest.php
@@ -12,18 +12,18 @@ class MoneyTest extends TestCase
 {
     public function testMultiplication(){
         $five = New Dollar(5);
-        self::assertEquals(New Dollar(10), $five->times(2));
-        self::assertEquals(New Dollar(15), $five->times(3));
+        $this->assertObjectEquals(New Dollar(10), $five->times(2));
+        $this->assertObjectEquals(New Dollar(15), $five->times(3));
     }
 
     public function testEquality(){
-        self::assertTrue((New Dollar(5))->equals(New Dollar(5)));
-        self::assertFalse((New Dollar(5))->equals(New Dollar(6)));
+        $this->assertTrue((New Dollar(5))->equals(New Dollar(5)));
+        $this->assertFalse((New Dollar(5))->equals(New Dollar(6)));
     }
 
     public function testFrancMultiplication(){
         $five = New Franc(5);
-        self::assertEquals(New Franc(10), $five->times(2));
-        self::assertEquals(New Franc(15), $five->times(3));
+        $this->assertObjectEquals(New Franc(10), $five->times(2));
+        $this->assertObjectEquals(New Franc(15), $five->times(3));
     }
 }
