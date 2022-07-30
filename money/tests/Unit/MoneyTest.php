7d078552945f4b076aaf0de698ee9adbeeabe19d
kurita_k <kensuke.kurita@gmail.com>
2022-07-30 21:19:50 +0900

    不要となった者たちを削除する

--- money/tests/Unit/MoneyTest.php
+++ money/tests/Unit/MoneyTest.php
@@ -19,24 +19,11 @@ class MoneyTest extends TestCase
     public function testEquality(){
         $this->assertTrue(Money::dollar(5, "USD")->equals(Money::dollar(5, "USD")));
         $this->assertFalse(Money::dollar(5, "USD")->equals(Money::dollar(6, "USD")));
-        $this->assertTrue((Money::franc(5, "CHF"))->equals(Money::franc(5, "CHF")));
-        $this->assertFalse((Money::franc(5, "CHF"))->equals(Money::franc(6, "CHF")));
         $this->assertFalse((Money::dollar(5, "USD"))->equals(Money::franc(5, "CHF")));
     }
-
-    public function testFrancMultiplication(){
-        $five = Money::franc(5, "CHF");
-        $this->assertObjectEquals(Money::franc(10, "CHF"), $five->times(2));
-        $this->assertObjectEquals(Money::franc(15, "CHF"), $five->times(3));
-    }
-
+    
     public function testCurrency(){
         $this->assertEquals('USD', Money::dollar(1, "USD")->currency());
         $this->assertEquals('CHF', Money::franc(1, "CHF")->currency());
-    }
-
-    public function testDifferentClassEquality()
-    {
-        $this->assertTrue((new Money(10, "CHF"))->equals(new Franc(10, "CHF")));
     }
 }
